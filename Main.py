348f6c89fc441890fb91ee3c7641c30fae2711cf
xharlesd <bernidocharlesdavid@gmail.com>
2023-07-23 19:43:04 +0800

    instructions, create pseudocode

---
+++ Main.py
@@ -0,0 +1,8 @@
+"""
+- Create a program that ask user for typical information found in covid contact tracing app
+- Write the collected information in a file (use any format you like)
+- The app should be able to add and search entry
+- Be creative, the realistic the better.
+- Please don't submit downloaded program
+- Please dont follow any online contact tracing programming tutorial
+"""
