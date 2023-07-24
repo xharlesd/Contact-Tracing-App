7969a46029dd6b009f6b4f73e12e9bfd58c22ac5
xharlesd <bernidocharlesdavid@gmail.com>
2023-07-24 16:08:05 +0800

    created contact tracing app window

--- Main.py
+++ Main.py
@@ -8,27 +8,34 @@
 """
 
 # import necessary modules to be used
+from typing import Optional, Tuple, Union
 import customtkinter as ctk
 import tkinter as tk
 # import classes (soon)
 
 
 # App background appearance
-ctk.set_appearance_mode("System")
+ctk.set_appearance_mode("Light")
 
 # App color theme
 ctk.set_default_color_theme("dark-blue") 
 
 # App Class
-class CTMainWindow(ctk.CTk):
+class ContactTracing(ctk.CTk):
     """"""
     # constructor
-    # program title
+    def __init__(self, fg_color: str | Tuple[str, str] | None = None, **kwargs):
+        super().__init__(fg_color, **kwargs)
 
-    # create tkinter frame window
-
+        self.title("Contact Tracing App")  # program title
+        self.geometry('900x600')  # tkinter frame window
+    
     # create label for add entry 
     # button for add entry
 
     # create label for search entry
     # button for search entry
+
+if __name__ == "__main__":
+    ContactTracing = ContactTracing()
+    ContactTracing.mainloop()
