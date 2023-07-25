7d11243f7a576fdb6526ef8321a4160685c8ec1e
xharlesd <bernidocharlesdavid@gmail.com>
2023-07-25 22:27:30 +0800

    created label and textboxes for respondent detail

--- Main.py
+++ Main.py
@@ -20,29 +20,29 @@ class ContactTracing():
     def __init__(self):
         self.window = tk.Tk()
         self.window.title("Contact Tracing App")  # program title
-        self.window.geometry('1000x700')  # tkinter frame window
+        self.window.geometry('1200x800')  # tkinter frame window
     
         red_frame = tk.Frame(bd=0, highlightthickness=0, background="#800000")
-        blue_frame = tk.Frame(bd=0, highlightthickness=0, background="white")
+        white_frame = tk.Frame(bd=0, highlightthickness=0, background="white")
         red_frame.place(x=0, y=0, relwidth=0.4, relheight=1, anchor="nw")
-        blue_frame.place(relx=0.35, y=0, relwidth=0.7, relheight=1, anchor="nw")
+        white_frame.place(relx=0.3, y=0, relwidth=0.7, relheight=1, anchor="nw")
 
         app_title = tk.Label(self.window, text = "PUP", fg = "#0818A8", bg = "white", font=("Arial",55,"bold"))
-        app_title.place(x = 430, y = 80, width = 250, height = 90)
+        app_title.place(x = 530, y = 120, width = 250, height = 90)
 
         app_title = tk.Label(self.window, text = "TRACE", fg = "red", bg = "white", font=("Arial",55,"bold"))
-        app_title.place(x = 630, y = 80, width = 250, height = 90)
+        app_title.place(x = 730, y = 120, width = 250, height = 90)
 
         app_title = tk.Label(self.window, text = "PUP CONTACT TRACING APP", fg = "gray", bg = "white", font=("Arial",20,"bold"))
-        app_title.place(x = 455, y = 155, width = 450, height = 20)
+        app_title.place(x = 555, y = 195, width = 450, height = 22)
 
         # button for add entry
-        add_info_Button = tk.Button(self.window, text = "Add Info", command = self.register_window, fg = "white", bg = "#40B5AD", font=("Century Gothic",24,"bold"))
-        add_info_Button.place(x = 550, y = 310, width = 250, height = 90)
+        add_info_Button = tk.Button(self.window, text = "REGISTER", command = self.register_window, fg = "white", bg = "#40B5AD", font=("Century Gothic",24,"bold"))
+        add_info_Button.place(x = 650, y = 340, width = 250, height = 90)
         
         # button for search entry
-        search_info_Button = tk.Button(self.window, text = "Search Data", fg = "white", bg = "#40B5AD", font=("Century Gothic",24,"bold"), )
-        search_info_Button.place(x = 550, y = 420, width = 250, height = 90)
+        search_info_Button = tk.Button(self.window, text = "SEARCH", fg = "white", bg = "#40B5AD", font=("Century Gothic",24,"bold"), )
+        search_info_Button.place(x = 650, y = 450, width = 250, height = 90)
 
         self.window.mainloop()
 
--- Obtain.py
+++ Obtain.py
@@ -12,34 +12,78 @@ class Register():
     def __init__(self):
         self.window = tk.Tk()
         self.window.title("Contact Tracing App")  # program title
-        self.window.geometry('1000x700')  # tkinter frame window
+        self.window.geometry('1200x800')  # tkinter frame window
 
-        # declare variables    
-        app_title = tk.Label(self.window, text = "PUP CONTACT TRACING APP", fg = "gray", bg = "white", font=("Arial",20,"bold"))
-        app_title.place(x = 455, y = 155, width = 450, height = 20)
-    
-    # tkinter window
+        # tkinter window
+        red_frame = tk.Frame(bd=0, highlightthickness=0, background="#800000")
+        white_frame = tk.Frame(bd=0, highlightthickness=0, background="white")
+        red_frame.place(x=0, y=0, relwidth=0.4, relheight=1, anchor="nw")
+        white_frame.place(relx=0.2, y=0, relwidth=0.8, relheight=1, anchor="nw")
+        
+        self.app_title = tk.Label(self.window, text = "Sign Up", fg = "black", bg = "white", font=("Helvetica",24,"bold"))
+        self.app_title.place(x = 265, y = 17, width = 120, height = 40)
 
-    # get name 
-    # textbox for name
+        self.app_title = tk.Label(self.window, text = "Respondent Details", fg = "#494F55", bg = "white", font=("Helvetica",15,"bold"))
+        self.app_title.place(x = 240, y = 87, width = 240, height = 22)
 
-    # get age
-    # textbox for age
+        # get name Name
+        self.name_label = tk.Label(self.window, text = "Name", fg = "black", bg = "white", font=("Trajan Pro",13))
+        self.name_label.place(x = 270, y = 125, width = 45, height = 20)
 
-    # get gender
-    # radio button for gender
+        # textbox for Name
+        self.name_textbox = tk.Entry(self.window, font=("MS Sans Serif",12), fg = "black", bg = "#F5F5F5")
+        self.name_textbox.place(x = 273 , y = 145, width = 230, height = 30)
+        # self.name_suffix_textbox.insert(0, " Suffix (e.g. Sr., Jr., III)")   
 
-    # get address
-    # textbox for address
+        # get Age 
+        self.age_label = tk.Label(self.window, text = "Age", fg = "black", bg = "white", font=("Trajan Pro",13))
+        self.age_label.place(x = 270, y = 190, width = 36, height = 20)
 
-    # get contact number
-    # textbox for contact number (11 digits only)
+        # textbox for Age
+        self.age_textbox = tk.Entry(self.window, font=("MS Sans Serif",12), fg = "black", bg = "#F5F5F5")
+        self.age_textbox.place(x = 273 , y = 210, width = 230, height = 30)
 
-    # get email address 
-    # textbox for email address
+        # get Birthday 
+        self.birthday_label = tk.Label(self.window, text = "Birthday", fg = "black", bg = "white", font=("Trajan Pro",13))
+        self.birthday_label.place(x = 270, y = 255, width = 65, height = 20)
 
-    # get contact number of contact person
-    # textbox for contact number (11 digits only)
+        # textbox for Birthday
+        self.birthday_textbox = tk.Entry(self.window, font=("MS Sans Serif",12), fg = "black", bg = "#F5F5F5")
+        self.birthday_textbox.place(x = 273 , y = 275, width = 230, height = 30)
+
+        # get email address 
+        self.email_address_label = tk.Label(self.window, text = "Email Address", fg = "black", bg = "white", font=("Trajan Pro",13))
+        self.email_address_label.place(x = 517, y = 125, width = 130, height = 20)
+
+        # textbox for email address
+        self.email_address_textbox = tk.Entry(self.window, font=("MS Sans Serif",12), fg = "black", bg = "#F5F5F5")
+        self.email_address_textbox .place(x = 530, y = 145, width = 230, height = 30)
+
+        # get contact number
+        self.contact_number_label = tk.Label(self.window, text = "Contact Number", fg = "black", bg = "white", font=("Trajan Pro",13))
+        self.contact_number_label.place(x = 522, y = 190, width = 130, height = 20)
+
+        # textbox for contact number
+        self.contact_number_textbox = tk.Entry(self.window, font=("MS Sans Serif",12), fg = "black", bg = "#F5F5F5")
+        self.contact_number_textbox .place(x = 530, y = 210, width = 230, height = 30)
+        
+        # get Gender 
+        self.gender_label = tk.Label(self.window, text = "Gender", fg = "black", bg = "white", font=("Trajan Pro",13))
+        self.gender_label.place(x = 525, y = 255, width = 60, height = 20)
+
+        # textbox for Gender
+        self.gender_textbox = tk.Entry(self.window, font=("MS Sans Serif",12), fg = "black", bg = "#F5F5F5")
+        self.gender_textbox.place(x = 530 , y = 275, width = 230, height = 30)
+
+        # get address
+        self.address_label = tk.Label(self.window, text = "Address", fg = "black", bg = "white", font=("Trajan Pro",13))
+        self.address_label.place(x = 272, y = 320, width = 65, height = 20)
+
+        # textbox for address
+        self.address_textbox = tk.Entry(self.window, font=("MS Sans Serif",12), fg = "black", bg = "#F5F5F5")
+        self.address_textbox .place(x = 273, y = 340  , width = 488, height = 30)
+
+        
 
     # get name of contact person
     # textbox for name of contact person
